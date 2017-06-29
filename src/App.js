a08f86a03823417bcfd92194f5404ee84f8e5546
Sonny <yihe939@gmail.com>
2017-06-29 11:29:05 -0400

    added API call

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            schools: []
+        };
+    }
     render() {
         return (
             <div className="App">
-                
+
             </div>
         );
+    }
+
+    componentDidMount() {
+        const endpoint = "http://www.scholarhood.ca/dev-test.json";
+        const schoolsData = [];
+
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(data => schoolsData.push(...data));
+
+        this.setState({
+            schools: schoolsData
+        });
     }
 }
 
